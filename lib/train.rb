289ca6414f82c9426285a25b73ca2afad1e36a5a
Kha Le & Andrew Lee <student@epicodus.com>
2015-05-06 11:08:11 -0700

    add a find method and save method to the Train class

--- lib/train.rb
+++ lib/train.rb
@@ -1,8 +1,9 @@
 class Train
-  attr_reader(:name)
+  attr_reader(:name, :id)
 
   define_method(:initialize) do |attributes|
     @name = attributes[:name]
+    @id = attributes[:id]
   end
 
   define_singleton_method(:all) do
@@ -15,5 +16,24 @@ class Train
     end
   trains_list
   end
-  
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO trains (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+
+  define_singleton_method(:find) do |identification|
+    found_train = nil
+    Train.all().each() do |train|
+      if train.id() == identification
+        found_train = train
+      end
+    end
+    found_train
+  end
+
+  define_method(:==) do |another_train|
+    self.name().==(another_train.name())
+  end
 end
--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -13,5 +13,22 @@ describe('Train') do
       expect(Train.all()).to(eq([]))
     end
   end
-  
+
+  describe('#save') do
+    it('will save the train into the train list') do
+      new_train = Train.new({:name => "South"})
+      new_train.save()
+      expect(Train.all()).to(eq([new_train]))
+    end
+  end
+
+  describe('.find') do
+    it('will find the train based on the id number') do
+      new_train = Train.new({:name => "South"})
+      new_train.save()
+      new_train2 = Train.new({:name => "North"})
+      new_train2.save()
+      expect(Train.find(new_train2.id())).to(eq(new_train2))
+    end
+  end
 end

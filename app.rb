edc48d3479c24fb7e4b6d8333e00a3ae5b7c8342
Kha Le & Andrew Lee <student@epicodus.com>
2015-05-06 15:28:49 -0700

    add all routes to sinatra and add trains to cities and vice versa

--- app.rb
+++ app.rb
@@ -5,8 +5,69 @@ require('./lib/city')
 also_reload('lib/**/*.rb')
 require('pg')
 
-DB = PG.connect({:dbname = 'train_system'})
+DB = PG.connect({:dbname => 'train_system'})
 
 get('/') do
+  @trains = Train.all()
+  @cities = City.all()
   erb(:index)
+end
+
+get('/trains') do
+  @trains = Train.all()
+  erb(:trains)
+end
+
+post('/trains') do
+  name = params.fetch('name')
+  train = Train.new({:name => name, :id => nil})
+  train.save()
+  @trains = Train.all()
+  erb(:trains)
+end
+
+
+
+get('/cities') do
+  @cities = City.all()
+  erb(:cities)
+end
+
+post('/cities') do
+  name = params.fetch('name')
+  city = City.new({:name => name, :id => nil})
+  city.save()
+  @cities = City.all()
+  erb(:cities)
+end
+
+get('/cities/:id') do
+  @city = City.find(params.fetch('id').to_i())
+  @trains = Train.all()
+  erb(:city_info)
+end
+
+
+get('/trains/:id') do
+  @train = Train.find(params.fetch('id').to_i())
+  @cities = City.all()
+  erb(:train_info)
+end
+
+patch('/trains/:id') do
+  train_id = params.fetch("id").to_i()
+  @train = Train.find(train_id)
+  cities_ids = params.fetch('city_ids')
+  @train.update({:cities_id => cities_ids})
+  @cities = City.all()
+  erb(:train_info)
+end
+
+patch('/cities/:id') do
+  cities_id = params.fetch("id").to_i()
+  @city = City.find(cities_id)
+  trains_ids = params.fetch('train_ids')
+  @city.update({:trains_id => trains_ids})
+  @trains = Train.all()
+  erb(:city_info)
 end
